9f8bc25a42d1347d3f6c2e1c856ecd1647e300b4
temportalflux <dustin.yost.t@gmail.com>
2016-02-02 15:35:31 -0500

    Add start for peewee database connection

--- inventory.py
+++ inventory.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, url_for, redirect
+from peewee import *
 
 app = Flask(__name__)
+database = SqliteDatabase('developmentData.db')
+
+#class Device(Model):
 
 @app.route('/')
 def index():
-	return render_template('inventory.html')
+	# http://flask.pocoo.org/snippets/15/
+
+	return render_template('inventory.html', inventoryData="", deviceLogData="")
 
 if __name__ == '__main__':
+	db.connect()
 	app.run()
