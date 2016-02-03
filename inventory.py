c85c721d2dcaea3bbe75468b7ec3c3140ab50b68
temportalflux <dustin.yost.t@gmail.com>
2016-02-03 14:37:19 -0500

    Add session test

--- inventory.py
+++ inventory.py
@@ -19,8 +19,9 @@ database = SqliteDatabase('developmentData.db')
 @app.route('/')
 def index():
 	# http://flask.pocoo.org/snippets/15/
-	#if 'username' in session:
-	return render_template('inventory.html', inventoryData="", deviceLogData="")
+	if 'username' in session:
+		return render_template('inventory.html', inventoryData="", deviceLogData="")
+	return "<p>no</p>"
 	#return redirect(url_for('login'));
 
 #@app.route('/login', methods=['GET', 'POST'])
