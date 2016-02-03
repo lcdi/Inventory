288f1e43deafe1fa3c87807dceb728a683d1e614
temportalflux <dustin.yost.t@gmail.com>
2016-02-03 16:14:28 -0500

    Trying new secret key setup

--- inventory.py
+++ inventory.py
@@ -12,7 +12,7 @@ import os.path
 app = Flask(__name__)
 
 # http://docs.peewee-orm.com/en/latest/peewee/quickstart.html
-database = SqliteDatabase('developmentData.db')
+#database = SqliteDatabase('developmentData.db')
 
 #class Device(Model):
 #	idNumber = IntField()
@@ -22,6 +22,26 @@ database = SqliteDatabase('developmentData.db')
 #	issues = TextField()
 #	photo = CharField()
 #	quality = CharField()
+
+# http://flask.pocoo.org/snippets/104/
+def install_secret_key(app, filename='secret_key'):
+    """Configure the SECRET_KEY from a file
+    in the instance directory.
+
+    If the file does not exist, print instructions
+    to create it from a shell with a random key,
+    then exit.
+
+    """
+    filename = os.path.join(app.instance_path, filename)
+    try:
+        app.config['SECRET_KEY'] = open(filename, 'rb').read()
+    except IOError:
+        print 'Error: No secret key. Create it with:'
+        if not os.path.isdir(os.path.dirname(filename)):
+            print 'mkdir -p', os.path.dirname(filename)
+        print 'head -c 24 /dev/urandom >', filename
+        sys.exit(1)
 
 @app.route('/')
 def index():
@@ -49,10 +69,11 @@ def logout():
 	return redirect(url_for('index'))
 
 # Load secret key
-with open('file.dat') as f:
-    app.secret_key = f.read()
+#with open('file.dat') as f:
+#    app.secret_key = f.read()
+install_secret_key(app)
 
 if __name__ == '__main__':
-	database.connect()
+	#database.connect()
 	app.run(debug = True)
 
