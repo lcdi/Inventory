4e8a689a966c5fa23f336828a6b54baa4447f856
temportalflux <dustin.yost.t@gmail.com>
2016-02-03 16:05:01 -0500

    Add secret key fucntion

--- inventory.py
+++ inventory.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, session, redirect, url_for, escape, re
 # Peewee
 from peewee import *
 # File manipulation
-import os
-from settings import APP_STATIC
+import sys
+import os.path
 # Other
 #from datetime import date
 
@@ -23,11 +23,22 @@ database = SqliteDatabase('developmentData.db')
 #	photo = CharField()
 #	quality = CharField()
 
+def install_secret_key(app, filename='secret_key'):
+    filename = os.path.join(app.instance_path, filename)
+    try:
+        app.config['SECRET_KEY'] = open(filename, 'rb').read()
+    except IOError:
+        print 'Error: No secret key. Create it with:'
+        if not os.path.isdir(os.path.dirname(filename)):
+            print 'mkdir -p', os.path.dirname(filename)
+        print 'head -c 24 /dev/urandom >', filename
+        sys.exit(1)
+
 @app.route('/')
 def index():
 	# http://flask.pocoo.org/snippets/15/
 	if 'username' in session:
-		return render_template('inventory.html', inventoryData="", deviceLogData="")
+		return render_template('inventory.html', name=escape(session['username']), inventoryData="", deviceLogData="")
 	return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -45,7 +56,7 @@ def logout():
 	session.pop('username', None)
 	return redirect(url_for('index'))
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LW/,?RT'
 
 if __name__ == '__main__':
 	db.connect()
