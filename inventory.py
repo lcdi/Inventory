cf5b7f743bb9707f9ae505a69b419eb46a66592a
temportalflux <dustin.yost.t@gmail.com>
2016-02-03 14:08:44 -0500

    Start testing peewee database connection

--- inventory.py
+++ inventory.py
@@ -6,11 +6,20 @@ app = Flask(__name__)
 # http://docs.peewee-orm.com/en/latest/peewee/quickstart.html
 database = SqliteDatabase('developmentData.db')
 
-#class Device(Model):
+class Device(Model):
+	idNumber = IntField()
+	serialNumber = CharField()
+	typeCategory = CharField()
+	description = TextField()
+	issues = TextField()
+	photo = CharField()
+	quality = CharField()
 
 @app.route('/')
 def index():
 	# http://flask.pocoo.org/snippets/15/
+	for item in Device.select():
+		print(item.serialNumber)
 
 	return render_template('inventory.html', inventoryData="", deviceLogData="")
 
