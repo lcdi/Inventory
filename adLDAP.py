499bcbf8c280302e81c8e106ce0a78dd690169ae
temportalflux <dustin.yost.t@gmail.com>
2016-03-09 19:46:53 +0000

    Fix adLDAP search filter

--- adLDAP.py
+++ adLDAP.py
@@ -25,20 +25,47 @@ def checkCredentials(controller, domainA, domainB, username, password):
 		return ('Wrong Credentials', False)
 	except ldap.SERVER_DOWN:
 		return ('Server Down', False)
-	return (True, True)
+	
+	#print(ldap_client.whoami_s())
+	
 	hasEditAccess = False
-	dn = 'cn=Users,' + base_dn
-	filter = 'cn=' + str(username)
+	dn = 'ou=Users,' + base_dn
+	dn = base_dn
+	#dn = 'cn=' + username + ',' + base_dn
+	#print(dn)
+	
+	filter = 'cn=' + username
+	filter = '(&(objectclass=person)(cn=%s)' % username
+	filter = '(uid=*)'
+	filter = 'samaccountname=' + username
+	#filter = ldap_filter
+	#filter = '(&(objectCategory=person)(%s))' % filter
+	#filter = 'memberOf=' + validEditAccessGroups[0]
+	#filter = 'cn=' + username
+	#print(filter)
+	
 	attrs = ['memberOf']
-	id = ldap_client.search(dn, ldap.SCOPE_SUBTREE, filter, attrs)
-	groups = ldap_client.result(id)[1][0][1]['memberOf']
+	
+	result = ldap_client.search_s(dn, ldap.SCOPE_SUBTREE, filter, attrs)
+	#print(result)
+	#for d1 in result:
+	#	print(d1)
+	groups = result[0][1]['memberOf']
+	#print(groups)
+	
+	#return ("", "")
+	#groups = ldap_client.result(id)[1][0][1]['memberOf']
 	for group in groups:
 		address = group.split(',')
 		groupName = address[0].split('=')[1]
 		if groupName in validEditAccessGroups:
 			hasEditAccess = True
 			break
+	#print(hasEditAccess)
 	
 	ldap_client.unbind()
 	return (True, hasEditAccess)
 	
+#checkCredentials("research-dc", "research", "lcdi", "dyost", "D4rKcH0c014+3$!")
+#print("")
+#checkCredentials("research-dc", "research", "lcdi", "amaccarone", "DiMo17*71")
